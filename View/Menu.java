e4c5aa939fec513568e03ff7f3399374475f4c2b
Santiago García <santiagogarciaruiz665@gmail.com>
2024-02-07 11:02:20 +0100

    Menu y Main Controller

--- View/Menu.java
+++ View/Menu.java
@@ -1,9 +1,13 @@
 package View;
 
 public class Menu {
-    System.out.println("1. Add Favorite");
-    System.out.println("2. Remove Favorite");
-    System.out.println("3. Update Favorite");
-    System.out.println("4. Find Favorite");
-    System.out.println("5. Exit");
+    public static void showMenu() {
+        System.out.println("Welcome to the library Trenosaurius!");
+        System.out.println("1. Add favorite"); // ADD FAVORITE, UPDATE FAVORITE.
+        System.out.println("2. Add Movie, Song or Game"); // ADD MOVIE, ADD SONG, ADD GAME.
+        System.out.println("3. Show favorites");
+        System.out.println("4. Find favorite"); // ALL, ID, CLASS, NAME.
+        System.out.println("5. Delete favorite"); // BY ID, BY NAME.
+        System.out.println("6. Exit");
+    }
 }
